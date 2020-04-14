a9c52b4b9876cdfa487066bc24c764108dfa0fd7
Jeremy Kale Padot <jeremy@padot.us>
2020-04-14 11:54:08 -0700

    write theroys on functional programming and templates
    Co-authored-by: Patrick Delaney <prestwick97@gmail.com>
    
    Co-authored-by: Benjamin Thom <jaminthom@gmail.com>

--- src/homework.js
+++ src/homework.js
@@ -29,7 +29,7 @@
 //   }
 // }
 
-//Refactor v2
+//Refactor v2 (function factory example)
 
 const changeState = (prop) => {
   return (value) => {
@@ -48,3 +48,24 @@ const blueFood = changeState("soil")(5)
 const greenFood = changeState("soil")(10)
 const yuckyFood = changeState("soil")(-5)
  
+
+//  function for storing state
+
+const storeState = () => {  
+  let currentState = {};    //storeState's only job is to store the currentState of an object
+  return (stateChangeFunction) => {   //will specify the exact change that should be made to currentState
+    const newState = stateChangeFunction(currentState); //creates a new version of currentState specific to what its doing
+    currentState = {...newState};   //each time our annon function is called (inner function stateChangeFunction) our currentState (inner function of our outer function storeState) will be mutated // updates currentState. makes a copy of newState and assignes it to currentState using spread opperator
+    return newState;   //a newState is returned by our inner function
+  }
+}
+// Theroy: Functional programming allows us to build out template to be used in many different cases. 
+// Step one seems to be: build a template, 2: build a new version of that function to have a specific use. 
+
+const stateChanger = storeState();
+
+(stateaChangerFunction) => {
+  const newState = stateChangeFunction(currentState);
+  currentState = {...newState};
+  return newState;
+}
