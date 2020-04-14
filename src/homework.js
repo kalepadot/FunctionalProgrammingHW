2601e718e6f2ea0219fdca29491277359937f354
Jeremy Kale Padot <jeremy@padot.us>
2020-04-14 11:25:23 -0700

    complet part one, create pure function
    Co-authored-by: Patrick Delaney <prestwick97@gmail.com>
    Co-authored-by: Benjamin Thom <jaminthom@gmail.com>

--- src/homework.js
+++ src/homework.js
@@ -1,37 +1,38 @@
 //Functional Approach
 
-//No refactoring v0:
+// //No refactoring v0:
 
-const hydrate = (plant) => {
-  return {
-    ...plant,
-    water: (plant.water || 0) + 1
-  }
-};
+// const hydrate = (plant) => {
+//   return {
+//     ...plant,
+//     water: (plant.water || 0) + 1
+//   }
+// };
 
-const feed = (plant) => {
-  return {
-    ...plant,
-    soil: (plant.soil || 0) +1
-  }
-};
+// const feed = (plant) => {
+//   return {
+//     ...plant,
+//     soil: (plant.soil || 0) +1
+//   }
+// };
 
-> let plant = { soil: 0, light: 0, water: 0 }
-> changePlantState(plant, "soil")
-{soil: 1, light: 0, water: 0}
+// > let plant = { soil: 0, light: 0, water: 0 }
+// > changePlantState(plant, "soil")
+// {soil: 1, light: 0, water: 0}
 
-//Refactor v1:
+// //Refactor v1:
 
-const changePlantState = (state, prop, value) => {
-  return {
-    ...state,
-    [prop]: (state[prop] || 0) + value
-  }
-}
+// const changePlantState = (state, prop, value) => {
+//   return {
+//     ...state,
+//     [prop]: (state[prop] || 0) + value
+//   }
+// }
 
 //Refactor v2
 
 const changeState = (prop) => {
+
   return (value) => {
     return (state) => ({
       ...state,
@@ -40,4 +41,7 @@ const changeState = (prop) => {
   }
 }
 
+const feed = changState("soil");
+const hydrate = changeState("water");
+const giveLight = changeState("light");
  
